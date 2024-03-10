9878f7801a69332a31d6fbd582fa1673a2ca9469
anastazija-kovachevikj <anastazija.kovachevikj@gmail.com>
2024-03-10 17:25:13 +0100

    database

---
+++ Vinozito/Vinozito/Models/Avatar.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Vinozito.Models
+{
+    public class Avatar
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public String photo { get; set; }
+        public String name { get; set; }
+    }
+}
