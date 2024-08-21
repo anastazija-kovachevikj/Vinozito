c7ece25c0060541882d065d7c3fbb3d9fc347adc
mangarovska <ana.mangarovska@gmail.com>
2024-08-21 22:12:59 +0200

    models

---
+++ backend/backend/models/CustomCard.cs
@@ -0,0 +1,8 @@
+﻿namespace backend.models;
+
+public class CustomCard(string id, string defaultCardId, string voiceAudio)
+{
+    public string Id { get; set; } = id;
+    public string DefaultCardId { get; set; } = defaultCardId;
+    public string VoiceAudio { get; set; } = voiceAudio;
+}
---
+++ backend/backend/models/DefaultCard.cs
@@ -0,0 +1,9 @@
+﻿namespace backend.models;
+
+public class DefaultCard(string id, string audioVoice, string image, string category)
+{
+    public string Id { get; set; } = id;
+    public string AudioVoice { get; set; } = audioVoice;
+    public string Image { get; set; } = image;
+    public string Category { get; set; } = category;
+}
