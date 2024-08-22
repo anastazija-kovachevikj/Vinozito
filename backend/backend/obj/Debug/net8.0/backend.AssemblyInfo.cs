9a32f097b413eb0af78851997142b3e6901d90af
mangarovska <ana.mangarovska@gmail.com>
2024-08-22 15:37:35 +0200

    models

--- backend/backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ec0376bbb836954b891d8eb4ee4522e53ae5ea1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7ece25c0060541882d065d7c3fbb3d9fc347adc")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
