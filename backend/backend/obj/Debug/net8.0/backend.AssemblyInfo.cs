78ac8c4b13cb7cb5b866fdfaf7f7e6c1d15c8007
Anastazija Kovacevic <anastazija.kovachevikj@gmail.com>
2024-08-25 17:53:06 +0200

    Remove build artifacts from Git tracking

--- backend/backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a862af0bff4752fd0006387007de65f99b5e7d64")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3fddd55a2bc95bad5273bdc2addbf47587db332")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
