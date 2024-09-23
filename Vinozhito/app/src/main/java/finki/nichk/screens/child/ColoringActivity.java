b407d12e2b6b3e600a0817235453896a77342d08
mangarovska <ana.mangarovska@gmail.com>
2024-09-23 23:19:06 +0200

    editing, coloring background, comms tab

--- Vinozhito/app/src/main/java/finki/nichk/screens/child/ColoringActivity.java
+++ Vinozhito/app/src/main/java/finki/nichk/screens/child/ColoringActivity.java
@@ -2,51 +2,41 @@ package finki.nichk.screens.child;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.graphics.Path;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.widget.ImageButton;
 
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.app.AppCompatDelegate;
 
 import finki.nichk.R;
 
 public class ColoringActivity extends AppCompatActivity {
-    static {
-        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
-    }
-
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.coloring_main_menu);
+        setContentView(R.layout.coloring_main_menu); // to be updated to coloring_layout
 
         ImageButton imageButton1 = findViewById(R.id.imageButton_ladybug);
-        ImageButton imageButton2 = findViewById(R.id.imageButton_dino);
+//        ImageButton imageButton2 = findViewById(R.id.imageButton_dino);
         ImageButton imageButton3 = findViewById(R.id.imageButton_cake);
         ImageButton imageButton4 = findViewById(R.id.imageButton_butterfly);
         ImageButton imageButton5 = findViewById(R.id.imageButton_rainbow);
         ImageButton imageButton6 = findViewById(R.id.imageButton_cow);
         ImageButton imageButton7 = findViewById(R.id.imageButton_cactus);
-        ImageButton imageButton8 = findViewById(R.id.imageButton_cp);
+//        ImageButton imageButton8 = findViewById(R.id.imageButton_cp);
         ImageButton imageButton9 = findViewById(R.id.imageButton_ufo);
         ImageButton imageButton10 = findViewById(R.id.imageButton_horse);
         ImageButton backButton = findViewById(R.id.back_button);
 
-        imageButton1.setOnClickListener(v -> openColoringScreen(R.drawable.bug_vector));
-        imageButton2.setOnClickListener(v -> openColoringScreen(R.drawable.dino_vector));
-        imageButton3.setOnClickListener(v -> openColoringScreen(R.drawable.cake_vector));
-        imageButton4.setOnClickListener(v -> openColoringScreen(R.drawable.butterfly_vector));
-        imageButton5.setOnClickListener(v -> openColoringScreen(R.drawable.rainbow_vector));
-        imageButton6.setOnClickListener(v -> openColoringScreen(R.drawable.cow_vector));
-        imageButton7.setOnClickListener(v -> openColoringScreen(R.drawable.cactus));  // TREBA DA SE SMENI
-        imageButton8.setOnClickListener(v -> openColoringScreen(R.drawable.cp_vector));
-        imageButton9.setOnClickListener(v -> openColoringScreen(R.drawable.ufo_vector));
-        imageButton10.setOnClickListener(v -> openColoringScreen(R.drawable.horse_vector));
-
+        imageButton1.setOnClickListener(v -> openColoringScreen(R.drawable.ladybug));
+//        imageButton2.setOnClickListener(v -> openColoringScreen(R.drawable.dino));
+        imageButton3.setOnClickListener(v -> openColoringScreen(R.drawable.cake));
+        imageButton4.setOnClickListener(v -> openColoringScreen(R.drawable.butterfly));
+        imageButton5.setOnClickListener(v -> openColoringScreen(R.drawable.rainbow));
+        imageButton6.setOnClickListener(v -> openColoringScreen(R.drawable.cow));
+        imageButton7.setOnClickListener(v -> openColoringScreen(R.drawable.cactus));
+//        imageButton8.setOnClickListener(v -> openColoringScreen(R.drawable.cp));
+        imageButton9.setOnClickListener(v -> openColoringScreen(R.drawable.ufo));
+        imageButton10.setOnClickListener(v -> openColoringScreen(R.drawable.horse));
 
         buttonTouchListener(backButton, () -> {
             Intent intent = new Intent(ColoringActivity.this, ChildActivity.class);
